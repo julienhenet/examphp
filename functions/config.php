28f5dd463e11dd1e54194b35dd3b3a0c5fa4ab72
julienhenet <julienhenet@gmail.com>
2014-06-15 22:48:46 +0200

    New confi.php

--- functions/config.php
+++ functions/config.php
@@ -1,9 +1,9 @@
 <?php
 	//ficher de connection à la DB
-	$dbhost = 'weasrlistlogin.mysql.db';
-	$dbuser = 'weasrlistlogin';
-	$dbpass = 'eamDbCxRPMY4';
-	$db		= 'weasrlistlogin'; 
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$db		= 'tfetest'; 
 	$conn 	= mysql_connect($dbhost, $dbuser, $dbpass) or die("Impossible de se connecter à la base de donnée :(");
 	mysql_select_db($db) or die("La base de donnée est introuvable :(");
 	define("BASE_URL", "http://weasr.com/mailinglist/index.php"); 
