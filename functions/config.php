eb491da83e82ddd39c0a6d789ab3ffdfb291e2a7
Alexandre Plennevaux <pixeline@users.noreply.github.com>
2014-06-15 20:35:13 +0200

    hide database credentials
    come on...

--- functions/config.php
+++ functions/config.php
@@ -1,9 +1,9 @@
 <?php
 	//ficher de connection à la DB
-	$dbhost = 'weasrlistlogin.mysql.db';
-	$dbuser = 'weasrlistlogin';
-	$dbpass = 'eamDbCxRPMY4';
-	$db		= 'weasrlistlogin'; 
+	$dbhost = 'xxxx.mysql.db';
+	$dbuser = 'xxxx';
+	$dbpass = 'xxxx';
+	$db		= 'xxxx'; 
 	$conn 	= mysql_connect($dbhost, $dbuser, $dbpass) or die("Impossible de se connecter à la base de donnée :(");
 	mysql_select_db($db) or die("La base de donnée est introuvable :(");
 	define("BASE_URL", "http://weasr.com/mailinglist/index.php"); 
